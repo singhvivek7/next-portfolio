fe270cc4c4e6a620cda96df06840c644bfbcc57e
ViveK <vivekartist9999@gmail.com>
2024-05-19 12:33:22 +0530

    feat: handle contact form and send mail, add footer

---
+++ src/config/app.config.ts
@@ -0,0 +1,9 @@
+export const appConfig = {
+  emailFrom: process.env.EMAIL_FROM ?? '',
+  emailTo: process.env.EMAIL_TO ?? '',
+  emailUser: process.env.EMAIL_USER ?? '',
+  emailPass: process.env.EMAIL_PASS ?? '',
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL ?? '',
+  recaptchaSiteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? '',
+  recaptchaSecret: process.env.NEXT_PUBLIC_RECAPTCHA_SECRET ?? '',
+};
---
+++ src/config/request.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { appConfig } from '@/config//app.config';
+
+const axiosInstance = axios.create({
+  baseURL: appConfig.baseUrl,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    return Promise.reject(error.response.data);
+  }
+);
+
+export const postReq = async (url: string, reqBody: AnyType) => {
+  const { data } = await axiosInstance.post(url, reqBody);
+
+  return data;
+};
