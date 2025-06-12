92d39755b726eeb80dbbd8df06257351642248cf
Vivek Singh <153614040+singhvivek7@users.noreply.github.com>
2025-06-12 21:48:26 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,12 @@ import { TooltipProvider } from '@/components/ui/tooltip'
 import { DATA } from '@/data/resume'
 import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
-import { Inter as FontSans } from 'next/font/google'
+import { Roboto as FontSans } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
 
 const fontSans = FontSans({
+  weight: ['400'],
   subsets: ['latin'],
   variable: '--font-sans',
 })
