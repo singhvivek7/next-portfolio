bcd0436948bf89627770071de8720124a52d731e
Vivek Singh <153614040+singhvivek7@users.noreply.github.com>
2025-06-12 21:46:31 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -85,7 +85,7 @@ export const DATA: DataType = {
     {
       company: 'Traya Health',
       href: 'https://traya.health',
-      badges: ['health'],
+      badges: [],
       location: 'Bengaluru, Karnataka',
       title: 'Software Development Engineer - I',
       logoUrl: '/traya.avif',
@@ -97,7 +97,7 @@ export const DATA: DataType = {
     {
       company: 'PayBolt Technologies',
       href: 'https://paybolt.in',
-      badges: ['fintech'],
+      badges: [],
       location: 'Bengaluru, Karnataka',
       title: 'Software Development Engineer - I',
       logoUrl: '/paybolt.png',
